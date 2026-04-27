190f4afe9155a48baf3a2e0235061cf3a19e2020
Kirill Fedulin <crazzyhd22@gmail.com>
2026-04-27 09:35:11 +0300

    Update triggerFedulin.sql

--- triggerFedulin.sql
+++ triggerFedulin.sql
@@ -11,26 +11,75 @@ rahvaarv int not null);
 CREATE TABLE logi(
 ID int primary key identity(1,1),
 kuupaev datetime,
-andmed TEXT);
+andmed TEXT,
+kasutaja varchar(25));
 
---Insert Triger
+--tabel maakonnad
+CREATE TABLE maakonnad(
+maakondID int primary key identity(1,1),
+maakondNimi varchar(25) UNIQUE);
+
+--foreign key tabelis linnad
+ALTER TABLE linnad ADD maakondID int;
+SELECT * FROM linnad;
+ALTER TABLE linnad ADD CONSTRAINT fk_maakond
+FOREIGN KEY (maakondID) REFERENCES maakonnad(maakondID)
+--täidame tabelid
+--maakonnad
+INSERT INTO maakonnad
+VALUES('Harjumaa'), ('Pärnumaa'), ('Virumaa');
+
+SELECT * FROM maakonnad;
+INSERT INTO linnad (linnanimi, rahvaarv, maakondID)
+VALUES ('Tallinn', 600000, 1), ('Rakvere', 150000, 3);
+
+Select * from linnad, maakonnad 
+WHERE linnad.maakondID=maakonnad.maakondID;
+-- sama päring inner join'iga
+Select * from linnad inner join maakonnad 
+ON linnad.maakondID=maakonnad.maakondID;
+
+--Insert Triger, mis jälgib kaks seostatud tabelit
 CREATE TRIGGER linnaLisamine
 ON linnad
 FOR INSERT 
 AS 
-INSERT INTO logi(kuupaev, andmed)
+INSERT INTO logi(kuupaev, andmed, kasutaja)
 SELECT 
-getdate(), inserted.linnanimi
-FROM inserted;
+getdate(),
+CONCAT('lisatatud linn: ',inserted.linnanimi, ', ', inserted.rahvaarv, ', ', m.maakondNimi),
+SYSTEM_USER
+FROM inserted INNER JOIN maakonnad m
+ON inserted.maakondID=m.maakondID;
 
 --kontrollimiseks tuleb lisada uus linn tabelisse linnad
-INSERT INTO linnad(linnanimi, rahvaarv)
-VALUES ('Test', 1111);
+INSERT INTO linnad(linnanimi, rahvaarv, maakondID)
+VALUES ('Paide', 100000, 2);
 SELECT * FROM linnad;
 SELECT * FROM logi;
 
---kustutamine triger
+
+--triger mis jälgib andmete kustutamine seotud tabelite põhjal
+CREATE TRIGGER linnaKustutamine
+ON linnad
+FOR DELETE
+AS 
+INSERT INTO logi(kuupaev, andmed, kasutaja)
+SELECT 
+getdate(),
+CONCAT('kustutatud linn ',deleted.linnanimi, ', ', deleted.rahvaarv, ', ', m.maakondNimi),
+SYSTEM_USER
+FROM deleted INNER JOIN maakonnad m
+ON deleted.maakondID=m.maakondID;
+
+--kontroll
+DELETE FROM linnad where linnID=1;
+select * from logi;
+Drop trigger linnaKustutamine;
 drop trigger linnaLisamine
+
+
+
 
 --teine triger
 CREATE TRIGGER linnaLisamine
@@ -43,39 +92,29 @@ getdate(),
 CONCAT('lisatud linn: ', inserted.linnanimi, ' ! rahvaarv: ', inserted.rahvaarv, ' ! id: ', inserted.linnID)
 FROM inserted;
 
---DELETE TRIGGER
-CREATE TRIGGER linnaKustutamine
-ON linnad
-FOR DELETE
-AS 
-INSERT INTO logi(kuupaev, andmed)
-SELECT 
-getdate(), 
-CONCAT('kustutatud linn: ', deleted.linnanimi, ' ! rahvaarv: ', deleted.rahvaarv, ' ! id: ', deleted.linnID)
-FROM deleted;
-
+--kontroll
 delete from linnad where linnID=1;
 select * from linnad;
 select * from logi;
 
 --UPDATE TRIGGER
-CREATE TRIGGER linnaParandamine
+CREATE TRIGGER linnaUuendamine
 ON linnad
 FOR UPDATE
 AS 
-INSERT INTO logi(kuupaev, andmed)
+INSERT INTO logi(kuupaev, andmed, kasutaja)
 SELECT 
-getdate(), 
-CONCAT('kustutatud linn: ', d.linnanimi, ' ! rahvaarv: ', d.rahvaarv, ' ! id: ', d.linnID, 'i linn: ', i.linnanimi, ' ! rahvaarv: ', i.rahvaarv, ' ! id: ', i.linnID)
-FROM deleted d inner join inserted i
-on d.linnID=i.linnID;
+getdate(),
+CONCAT(
+'kustutatud linn ',deleted.linnanimi, ', ', deleted.rahvaarv, ', ', m1.maakondNimi,
+'|| uue linn ',inserted.linnanimi, ', ', inserted.rahvaarv, ', ', m2.maakondNimi),
+SYSTEM_USER
+FROM deleted 
+INNER JOIN inserted on deleted.linnID=inserted.linnID
+INNER JOIN maakonnad m1 on deleted.maakondID=m1.maakondID
+INNER JOIN maakonnad m2 on deleted.maakondID=m2.maakondID;
 
 --kontrollimiseks uuendame linna andmed 
 SELECT * FROM linnad;
-UPDATE linnad set linnanimi='Tap uus', rahvaarv=26
-where linnID=2;
-select * from linnad;
-select * from logi;
-
---lisame kasutajanimi logi tabelisse
-ALTER TABLE logi ADD kasutaja varchar(40);
+SELECT * FROM maakonnad
+UPDATE linnad set maakondId=1 where linnID=5;
